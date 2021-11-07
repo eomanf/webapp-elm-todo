012729edaae53668f14e53ae2d9b26335d0cf700
eomanf <eomanf@protonmail.com>
2021-11-07 22:53:32 +0800

    Add placeholder for MVU

--- src/Main.elm
+++ src/Main.elm
@@ -5,3 +5,46 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 
+
+
+-- Main
+
+
+main =
+    Browser.sandbox
+        { init = init, view = view, update = update }
+
+
+
+-- Model
+
+
+type alias Model =
+    {}
+
+
+init : Model
+init =
+    {}
+
+
+
+-- Update
+
+
+type alias Msg =
+    String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    Debug.todo "Implement update"
+
+
+
+-- View
+
+
+view : Model -> Html Msg
+view model =
+    Debug.todo "Implement view"
